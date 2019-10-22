a7b3c8a8f2f8f1fb90eb96d074dcd5e32d3916e1
delpiero ramos ramos <delpiee@hotmail.com>
2019-10-21 21:36:53 -0500

    Agregar archivos de proyecto.

---
+++ MVVM/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using MVVM.Views;
+namespace MVVM
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Item1.Clicked += Item1_Clicked;
+        }
+
+        private void Item1_Clicked(object sender, EventArgs e)
+        {
+            ((NavigationPage)this.Parent).PushAsync(new Operaciones());
+        }
+    }
+}
---
+++ MVVM/Views/Operaciones.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MVVM.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MVVM.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Operaciones : ContentPage
+    {
+        public Operaciones()
+        {
+            InitializeComponent();
+            this.BindingContext = new OperacionesViewModel();
+        }
+    }
+
+}
